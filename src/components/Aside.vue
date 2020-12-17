6e71e07c01e8abe7f4bc450f98363c8b00a01882
Chandra Okktaviani <candraoktaviani2017@gmail.com>
2020-12-17 09:39:35 +0700

    adding aside

---
+++ src/components/Aside.vue
@@ -0,0 +1,37 @@
+<template>
+  <vue-card-stack :cards="cards" :stack-width="240" :card-width="250">
+    <template v-slot:card="{ card }">
+      <div
+        style="width: 100%; height: 100%;"
+        :style="{ background: card.background }"
+      ></div>
+    </template>
+  </vue-card-stack>
+  <!-- <h1 class="i">You</h1> -->
+</template>
+<script>
+// import Vue from 'vue'
+import VueCardStack from 'vue-card-stack'
+export default {
+  components: {
+    VueCardStack
+  },
+  data() {
+    return {
+      cards: [
+        { background: '#00659d' },
+        { background: '#00abbc' },
+        { background: '#e2c58a' },
+        { background: '#fc8890' },
+        { background: '#b35d7f' }
+      ]
+    }
+  }
+}
+</script>
+<style scoped>
+.i {
+  background-color: seagreen;
+  margin: 20px 0;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,40 @@
 <template>
   <div class="home">
     <Header />
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <b-container class="bv-example-row">
+      <b-row>
+        <!-- <Aside b-col md="4" sm="12" bg-variant="info" class="a" /> -->
+        <b-col md="4" sm="12" bg-variant="info" class="a">
+          <Aside />
+        </b-col>
+        <b-col md="8" sm="12" bg-variant="primary" class="b">2 of 3</b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Header from '../components/_base/Header.vue'
-import HelloWorld from '@/components/HelloWorld.vue'
+import Aside from '../components/Aside.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
     Header,
-    HelloWorld
+    Aside
   }
 }
 </script>
+<style scoped>
+.a {
+  background-color: rosybrown;
+  border-right: 1px solid red;
+}
+.b {
+  background-color: sandybrown;
+}
+</style>
