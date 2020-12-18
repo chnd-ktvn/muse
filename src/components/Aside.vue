2dd86e0413cf7aa0d7afbb5e13a79d04deb8a112
Chandra Okktaviani <candraoktaviani2017@gmail.com>
2020-12-18 10:18:27 +0700

    adding tag main in Home.vue

--- src/components/Aside.vue
+++ src/components/Aside.vue
@@ -1,12 +1,56 @@
 <template>
-  <vue-card-stack :cards="cards" :stack-width="240" :card-width="250">
-    <template v-slot:card="{ card }">
-      <div
-        style="width: 100%; height: 100%;"
-        :style="{ background: card.background }"
-      ></div>
-    </template>
-  </vue-card-stack>
+  <div>
+    <b-container>
+      <b-row>
+        <b-col sm="12" md="6" lg="12">
+          <b-container>
+            <b-collapse id="my-collapse">
+              <vue-card-stack
+                :cards="cards"
+                :stack-width="240"
+                :card-width="250"
+                class="card-stack"
+              >
+                <template v-slot:card="{ card }">
+                  <div
+                    style="width: 100%; height: 100%; color: white; border-radius: 20px"
+                  >
+                    <b-card :style="{ background: card.background }">
+                      <b-container class="b-container-card">
+                        <img src="../assets/image 2.png" alt="" />
+                        <b-card-text class="b-card-text">
+                          Beef Spaghetti <br />20% OFF<br />
+                          Buy 1 Choco Oreo and get 20% off <br />
+                          for Beef Spaghetti <br />
+                          COUPON CODE <br />
+                          FNPR15RG <br />
+                          Valid untill October 10th 2020
+                        </b-card-text>
+                      </b-container>
+                    </b-card>
+                  </div>
+                </template>
+                <template v-slot:nav="{ onNext, onPrevious }">
+                  <nav class="nav">
+                    <button v-on:click="onPrevious" class="button a">
+                      <b-icon icon="chevron-left"></b-icon>
+                    </button>
+                    <button v-on:click="onNext" class="button b">
+                      <b-icon icon="chevron-right"></b-icon>
+                    </button>
+                  </nav>
+                </template>
+              </vue-card-stack>
+            </b-collapse>
+          </b-container>
+        </b-col>
+        <b-col sm="12" md="6" lg="12">
+          <b-button v-b-toggle.my-collapse>Apply Coupon</b-button>
+          <pre>Terms and Condition <br>1. You can only apply 1 coupon per day <br>2. It only for dine in <br>3. Buy 1 get 1 only for new user <br>4. Should make member card to <br>apply coupon</pre>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
   <!-- <h1 class="i">You</h1> -->
 </template>
 <script>
@@ -32,6 +76,42 @@ export default {
 <style scoped>
 .i {
   background-color: seagreen;
-  margin: 20px 0;
+  margin: 0 20px;
+  /* margin-bottom: 20px; */
+}
+vue-card-stack.card-stack {
+  position: relative;
+  margin-bottom: 20px;
+  border-radius: 20px;
+}
+.button {
+  position: absolute;
+  /* background-color: transparent; */
+  border: none;
+}
+.a {
+  left: 0;
+  top: 50%;
+  z-index: 10;
+}
+.b {
+  top: 50%;
+  z-index: 10;
+  margin-left: 200px;
+}
+.b-container-card {
+  margin: 0 7%;
+}
+/* .b-card-text {
+}*/
+img {
+  border-radius: 100%;
+  object-fit: contain;
+  width: 100%;
+  object-position: center;
+  padding: 0 20%;
+}
+pre {
+  font-size: 15px;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,10 +6,54 @@
     <b-container class="bv-example-row">
       <b-row>
         <!-- <Aside b-col md="4" sm="12" bg-variant="info" class="a" /> -->
-        <b-col md="4" sm="12" bg-variant="info" class="a">
+        <b-col lg="4" md="12" sm="12" bg-variant="info" class="a">
           <Aside />
         </b-col>
-        <b-col md="8" sm="12" bg-variant="primary" class="b">2 of 3</b-col>
+        <b-col lg="8" sm="12" bg-variant="primary" class="b">
+          <main>
+            <b-container>
+              <b-nav tabs align="center">
+                <b-nav-item active>Favorite Product</b-nav-item>
+                <b-nav-item>Coffee</b-nav-item>
+                <b-nav-item>Non Coffee</b-nav-item>
+                <b-nav-item>Foods</b-nav-item>
+                <b-nav-item>Add-on</b-nav-item>
+                <b-nav-item-dropdown
+                  id="my-nav-dropdown"
+                  text="Sort By:"
+                  toggle-class="nav-link-custom"
+                  right
+                >
+                  <b-dropdown-item>Name</b-dropdown-item>
+                  <b-dropdown-item>Price</b-dropdown-item>
+                  <b-dropdown-item>Created By</b-dropdown-item>
+                </b-nav-item-dropdown>
+              </b-nav>
+              <b-row>
+                <b-col
+                  md="3"
+                  sm="12"
+                  v-for="(item, index) in products"
+                  :key="index"
+                >
+                  <b-card
+                    title="Card Title"
+                    img-src="https://picsum.photos/600/300/?image=25"
+                    img-alt="Image"
+                    img-top
+                    tag="article"
+                    class="mb-2"
+                  >
+                    <b-card-text>
+                      Rp. {{ item.product_price }} <br />
+                      {{ item.product_name }}
+                    </b-card-text>
+                  </b-card>
+                </b-col>
+              </b-row>
+            </b-container>
+          </main>
+        </b-col>
       </b-row>
     </b-container>
   </div>
@@ -20,12 +64,49 @@
 import Header from '../components/_base/Header.vue'
 import Aside from '../components/Aside.vue'
 // import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'Home',
   components: {
     Header,
     Aside
+  },
+  data() {
+    return {
+      products: []
+    }
+  },
+  created() {
+    this.getProduct()
+  },
+  methods: {
+    getProduct() {
+      axios
+        .get(
+          `http://localhost:3050/product?orderBy=product_price&page=1&limit=10`
+        )
+        .then(response => {
+          console.log(response)
+          this.products = response.data.data
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    getCategory() {
+      axios
+        .get(
+          `http://localhost:3050/product?orderBy=category_id&page=1&limit=10`
+        )
+        .then(response => {
+          console.log(response)
+          this.products = response.data.data
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
   }
 }
 </script>
